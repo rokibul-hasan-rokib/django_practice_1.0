ea4d9196a2989a8614bdeb977e68ee48b5f5d342
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-02-08 09:42:45 +0600

    update form

--- blog/views.py
+++ blog/views.py
@@ -16,3 +16,14 @@ def blog_create(request):
     else:
         form = BlogForm()
     return render(request, 'client/pages/blog/form.html', {'form': form})
+
+def blog_update(request, pk):
+    blog = get_object_or_404(Blog, pk=pk)
+    if request.method == 'POST':
+        form = BlogForm(request.POST, request.FILES, instance=blog)
+        if form.is_valid():
+            form.save()
+            return redirect('blog_list')
+    else:
+        form = BlogForm(instance=blog)
+    return render(request, 'client/pages/blog/form.html', {'form': form})
