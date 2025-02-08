6e6ca28bbd05f0a521c25a8caff74fab2af0161a
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-02-08 09:59:06 +0600

    blog create

--- myproject/urls.py
+++ myproject/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('client/', include('client.urls')),  # Include client app URLs
+    path('client/', include('client.urls')),
+    path('blog/', include('blog.urls')),
 ]
 
