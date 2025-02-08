aaeff99efc0361e9415e4a3a4d6bd993390f13dc
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-02-08 11:49:55 +0600

    forms modified

--- service/forms.py
+++ service/forms.py
@@ -3,7 +3,7 @@ from .models import Service
 from blog.models import Blog 
 
 class ServiceForm(forms.ModelForm):
-    blog = forms.ModelChoiceField(queryset=Blog.objects.all(), label="Select Blog")
+    blog = forms.ModelChoiceField(queryset=Blog.objects.all(), label="Select Blog", empty_label="Select Blog", widget=forms.Select(attrs={'class': 'form-control'}))
 
     class Meta:
         model = Service
