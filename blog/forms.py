b1991be5a57c95472df88d6e7dc7120c7385d0e0
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-02-14 12:22:40 +0600

    form validation added

--- blog/forms.py
+++ blog/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from .models import Blog
 
 class BlogForm(forms.ModelForm):
+    title = forms.CharField(required=True, max_length=100, error_messages={'required': 'Title is required.'})
+    name = forms.CharField(required=True, max_length=100, error_messages={'required': 'Name is required.'})
+
     class Meta:
         model = Blog
         fields = ['name', 'title', 'description', 'image']
