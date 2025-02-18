dcf9d1c111d9bcbca74e00388a4d0e1cb93fc951
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-02-18 16:32:08 +0600

    category view index function

--- category/views.py
+++ category/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Category
+from .forms import CategoryForm
 
-# Create your views here.
+
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'client/pages/category/index.html', {'categories': categories})
