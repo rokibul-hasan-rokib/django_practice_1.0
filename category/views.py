f17b8dc07060fa73a5a0a14613728c736d530ec8
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-02-18 16:38:03 +0600

    view function done

--- category/views.py
+++ category/views.py
@@ -6,3 +6,36 @@ from .forms import CategoryForm
 def category_list(request):
     categories = Category.objects.all()
     return render(request, 'client/pages/category/index.html', {'categories': categories})
+
+
+def category_form(request, id=None):
+    if id:
+        category = get_object_or_404(Category, id=id)
+    else:
+        category = None
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, request.FILES, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('category_list')
+    else:
+        form = CategoryForm(instance=category)
+    return render(request, 'client/pages/category/form.html', {'form': form})
+
+def category_update(request, id):
+    category = get_object_or_404(Category, id=id)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, request.FILES, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('category_list')
+    else:
+        form = CategoryForm(instance=category)
+    return render(request, 'client/pages/category/form.html', {'form': form})
+
+def category_delete(request, id):
+    category = get_object_or_404(Category, id=id)
+    if request.method == 'POST':
+        category.delete()
+        return redirect('category_list')
+    return render(request, 'client/pages/category/delete.html', {'category': category})
