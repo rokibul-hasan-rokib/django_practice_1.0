faab490641cf9a3149e54720601bb7a2ada717fd
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-02-19 17:33:10 +0600

    client module validation form

--- client/forms.py
+++ client/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Client
 
 class ClientForm(forms.ModelForm):
+    "name" = forms.CharField(required=True, max_length=100, error_messages={'required': 'Name is required.'})
     class Meta:
         model = Client
         fields = ['name', 'designation']
