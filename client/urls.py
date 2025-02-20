6e7544c6ea7c2afcb491f00b11d38124dcd7f248
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-02-20 13:22:12 +0600

    api url fix

--- client/urls.py
+++ client/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import client_list, client_create, client_update, client_delete
+from .views import client_list, client_create, client_update, client_delete,ClientListCreateView,ClientRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('', client_list, name='client_list'),
     path('create/', client_create, name='client_create'),
     path('update/<int:pk>/', client_update, name='client_update'),
     path('delete/<int:pk>/', client_delete, name='client_delete'),
+    path('api/', ClientListCreateView.as_view(), name='client-list-create'),
+    path('api/<int:pk>/', ClientRetrieveUpdateDestroyView.as_view(), name='client-detail'),
 ]
