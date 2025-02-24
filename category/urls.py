db270611877d965099287639494922a51ada010d
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2025-02-24 14:56:13 +0600

    category list create view and details

--- category/urls.py
+++ category/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import category_list, category_form, category_update, category_delete
+from .views import category_list, category_form, category_update, category_delete,CategoryListCreateView,CategoryRetrieveUpdateDestroyView
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -8,4 +8,6 @@ urlpatterns = [
     path('create/', category_form, name='category_create'),
     path('update/<int:id>/', category_update, name='category_update'),
     path('delete/<int:id>/', category_delete, name='category_delete'),
+    path('api/', CategoryListCreateView.as_view(), name='category-list-create'),
+    path('api/<int:pk>/', CategoryRetrieveUpdateDestroyView.as_view(), name='category-detail'),
 ]
